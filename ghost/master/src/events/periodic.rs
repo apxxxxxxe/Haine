9692dda0320c8a5212c05badbe1d99ab4648661d
apxxxxxxe <calcium629@gmail.com>
2023-09-23 01:55:30 +0900

    update: ランダムトークできるように

--- ghost/master/src/events/periodic.rs
+++ ghost/master/src/events/periodic.rs
@@ -1,9 +1,15 @@
+use crate::events::aitalk::on_ai_talk;
 use crate::events::common::*;
 use crate::events::GlobalVariables;
 use shiorust::message::{Request, Response};
 
-pub fn on_second_change(_req: &Request, vars: &mut GlobalVariables) -> Response {
+pub fn on_second_change(req: &Request, vars: &mut GlobalVariables) -> Response {
     vars.total_time += 1;
     vars.ghost_up_time += 1;
-    new_response_nocontent()
+
+    if vars.ghost_up_time % vars.random_talk_interval == 0 {
+        on_ai_talk(req, vars)
+    } else {
+        new_response_nocontent()
+    }
 }
