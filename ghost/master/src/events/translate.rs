4265a1f4680133d55c9fa3dec6e42d35f6272445
apxxxxxxe <calcium629@gmail.com>
2023-09-26 21:55:26 +0900

    update: translate.rs: textonlytransaltor

--- ghost/master/src/events/translate.rs
+++ ghost/master/src/events/translate.rs
@@ -8,7 +8,32 @@ pub fn on_translate(text: String, vars: &mut GlobalVariables) -> String {
 
     let mut translated = text.clone();
 
+    translated = text_only_translater(translated, vars);
+
+    vars.volatility.inserter.run(translated)
+}
+
+// 参考：http://emily.shillest.net/ayaya/?cmd=read&page=Tips%2FOnTranslate%E3%81%AE%E4%BD%BF%E3%81%84%E6%96%B9&word=OnTranslate
+fn text_only_translater(text: String, vars: &mut GlobalVariables) -> String {
+    let re_tags = Regex::new(r"\\(\\|q\[.*?\]\[.*?\]|[!&8cfijmpqsn]\[.*?\]|[-*+1014567bcehntuvxz]|_[ablmsuvw]\[.*?\]|__(t|[qw]\[.*?\])|_[!?+nqsV]|[sipw][0-9])").unwrap();
+    let tags = re_tags.find_iter(&text);
+    let splitted = re_tags.split(&text).collect::<Vec<&str>>();
+    let mut result = String::new();
+
+    for (i, tag) in tags.enumerate() {
+        result.push_str(translate(splitted[i].to_string(), vars).as_str());
+        result.push_str(&tag.as_str());
+    }
+    result.push_str(translate(splitted[splitted.len() - 1].to_string(), vars).as_str());
+
+    result
+}
+
+fn translate(text: String, vars: &mut GlobalVariables) -> String {
     let surface_snippet = Regex::new(r"h([0-9]{7})").unwrap();
+
+    let mut translated = text.clone();
+
     translated = surface_snippet
         .replace_all(&translated, "\\0\\s[$1]")
         .to_string();
@@ -21,5 +46,5 @@ pub fn on_translate(text: String, vars: &mut GlobalVariables) -> String {
 
     translated = translated.replace("{user_name}", &vars.user_name.clone().unwrap());
 
-    vars.volatility.inserter.run(translated)
+    translated
 }
