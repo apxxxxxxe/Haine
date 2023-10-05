07bfc7aca92c19306f5d73e8985052949d1581f0
apxxxxxxe <calcium629@gmail.com>
2023-10-05 10:02:18 +0900

    update: mouse

--- ghost/master/src/events/mouse.rs
+++ ghost/master/src/events/mouse.rs
@@ -59,23 +59,19 @@ pub fn mouse_dialogs(info: String, vars: &mut GlobalVariables) -> Option<Vec<Str
             return Some(zero_bust_nade);
         }
         "0skirtup" => {
-            let mut zero_skirt_up = Vec::new();
+            let mut conbo_parts: Vec<Vec<String>> =
+                vec![vec!["h2244402……！\\nh1241102\\_w[500]".to_string()]];
             if !vars.volatility.first_sexial_touch && vars.volatility.ghost_up_time < 30 {
                 vars.volatility.first_sexial_touch = true;
-                zero_skirt_up.extend(all_combo(&vec![
-                    vec!["h2244402……！\\nh1241102\\_w[500]".to_string()],
-                    DIALOG_SEXIAL_FIRST.clone(),
-                ]));
+                conbo_parts.push(DIALOG_SEXIAL_FIRST.clone());
             } else {
-                zero_skirt_up.extend(all_combo(&vec![
-                    vec!["h2244402……！\\nh1241102\\_w[500]".to_string()],
-                    vec![
-                        "h1111204……いいもの見たって顔してる。h1111209屈辱だわ。".to_string(),
-                        "h1111205……ああ、ひどい人。h1111209泣いてしまいそうだわ。".to_string(),
-                        "h1111204……悪餓鬼。".to_string(),
-                    ],
-                ]));
+                conbo_parts.push(vec![
+                    "h1111204……いいもの見たって顔してる。h1111209屈辱だわ。".to_string(),
+                    "h1111205……ああ、ひどい人。h1111209泣いてしまいそうだわ。".to_string(),
+                    "h1111204……悪餓鬼。".to_string(),
+                ]);
             }
+            let zero_skirt_up: Vec<String> = all_combo(&conbo_parts);
             return Some(zero_skirt_up);
         }
         "0shoulderdown" => {
