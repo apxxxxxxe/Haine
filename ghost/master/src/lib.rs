286e0d93edf02bcb71a5fa0e0312f9a30dbc5311
apxxxxxxe <calcium629@gmail.com>
2023-11-10 00:10:46 +0900

    update: lib.rs

--- ghost/master/src/lib.rs
+++ ghost/master/src/lib.rs
@@ -7,19 +7,14 @@ mod variables;
 use crate::variables::get_global_vars;
 
 use std::fs::File;
-use std::path::Path;
 use std::panic;
+use std::path::Path;
 
 use shiori_hglobal::*;
 use shiorust::message::*;
 
 use winapi::ctypes::c_long;
-use winapi::shared::minwindef::{BOOL, DWORD, HGLOBAL, HINSTANCE, LPVOID, MAX_PATH, TRUE};
-use winapi::um::libloaderapi::GetModuleFileNameW;
-use winapi::um::winbase::GlobalFree;
-use winapi::um::winnt::{
-  DLL_PROCESS_ATTACH, DLL_PROCESS_DETACH, DLL_THREAD_ATTACH, DLL_THREAD_DETACH,
-};
+use winapi::shared::minwindef::{BOOL, HGLOBAL, TRUE};
 
 #[macro_use]
 extern crate log;
@@ -27,73 +22,31 @@ extern crate simplelog;
 
 use simplelog::*;
 
-static mut DLL_PATH: String = String::new();
-
 #[derive(Debug)]
 pub enum ResponseError {
   DecodeFailed,
 }
 
 #[no_mangle]
-pub extern "system" fn DllMain(
-  h_module: HINSTANCE,
-  ul_reason_for_call: DWORD,
-  _l_reserved: LPVOID,
-) -> BOOL {
-  match ul_reason_for_call {
-    DLL_PROCESS_ATTACH => {
-      register_dll_path(h_module);
-      let path;
-      unsafe {
-        path = Path::new(&DLL_PATH.clone())
-          .parent()
-          .unwrap()
-          .join("haine.log");
-      };
-      WriteLogger::init(
-        LevelFilter::Debug,
-        Config::default(),
-        File::create(path).unwrap(),
-      )
-      .unwrap();
-      debug!("DLL_PROCESS_ATTACH");
-    }
-    DLL_PROCESS_DETACH => {
-      debug!("DLL_PROCESS_DETACH");
-    }
-    DLL_THREAD_ATTACH => {}
-    DLL_THREAD_DETACH => {
-      debug!("DLL_THREAD_DETACH");
-    }
-    _ => {}
-  }
-  return TRUE;
-}
+pub extern "cdecl" fn load(h: HGLOBAL, len: c_long) -> BOOL {
+  let v = GStr::capture(h, len as usize);
+  let s = v.to_utf8_str().unwrap();
 
-fn register_dll_path(h_module: HINSTANCE) {
-  let mut buf: [u16; MAX_PATH + 1] = [0; MAX_PATH + 1];
-  unsafe {
-    GetModuleFileNameW(h_module, buf.as_mut_ptr(), MAX_PATH as u32);
-  }
+  get_global_vars().load();
 
-  let p = buf.partition_point(|v| *v != 0);
-
-  unsafe {
-    DLL_PATH = String::from_utf16_lossy(&buf[..p]);
-  }
-}
-
-#[no_mangle]
-pub extern "cdecl" fn load(h: HGLOBAL, _len: c_long) -> BOOL {
-  unsafe { GlobalFree(h) };
+  let log_path = Path::new(&s.to_string()).join("haine.log");
+  WriteLogger::init(
+    LevelFilter::Debug,
+    Config::default(),
+    File::create(log_path).unwrap(),
+  )
+  .unwrap();
 
   panic::set_hook(Box::new(|panic_info| {
     debug!("{}", panic_info);
   }));
 
   debug!("load");
-
-  get_global_vars().load();
 
   return TRUE;
 }
