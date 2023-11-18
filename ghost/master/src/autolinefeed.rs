b7686e83554b524b9c8743ba7ada281cb8f7874c
apxxxxxxe <calcium629@gmail.com>
2023-11-19 03:42:48 +0900

    update: autolinefeed: より網羅的に改行条件を見直し

--- ghost/master/src/autolinefeed.rs
+++ ghost/master/src/autolinefeed.rs
@@ -4,6 +4,19 @@ use std::sync::{Arc, Mutex};
 use std::thread::JoinHandle;
 use vibrato::{Dictionary, Tokenizer};
 
+#[derive(PartialEq, PartialOrd, Eq, Ord, Debug)]
+pub enum Rank {
+  First,
+  Second,
+  Manual,
+  Normal,
+}
+
+pub struct Block {
+  pub rank: Rank,
+  pub text: String,
+}
+
 pub struct Inserter {
   cols_num: f32,
   tokenizer: Arc<Mutex<Option<Tokenizer>>>,
@@ -45,26 +58,75 @@ impl Inserter {
     self.render(parts)
   }
 
-  fn wakachi(&mut self, src: String) -> Vec<String> {
-    println!("wakachi");
+  #[allow(dead_code)]
+  pub fn tokenize(&mut self, src: String) {
+    let tokenizer_clone = self.tokenizer.clone();
+    let tokenizer = tokenizer_clone.lock().unwrap();
+    let t = tokenizer.as_ref().unwrap();
+    let mut worker = t.new_worker();
+    worker.reset_sentence(&src);
+    worker.tokenize();
+    for token in worker.token_iter() {
+      println!("{}: {}", token.surface(), token.feature());
+    }
+  }
+
+  fn wakachi(&mut self, src: String) -> Vec<Block> {
     let tokenizer_clone = self.tokenizer.clone();
     let tokenizer = tokenizer_clone.lock().unwrap();
     let t = tokenizer.as_ref().unwrap();
     let mut worker = t.new_worker();
     let mut text = src.clone();
     let mut _word_counts = vec![0, 0];
-    let hinshi_target = vec![
-      "名詞",
-      "動詞",
-      "接頭詞",
-      "副詞",
-      "感動詞",
-      "形容詞",
-      "形容動詞",
-      "連体詞",
+
+    // 候補
+    // first: これが含まれていたら改行
+    let first = vec![
+      "動詞,自立",
+      "動詞,接尾",
+      "形容詞,自立",
+      "形容詞,接尾",
+      "形容詞,非自立",
+      "副詞,一般",
+      "副詞,助詞類接続",
+      "接続詞,",
+      "助詞,係助詞",
+      "助詞,終助詞",
+      "助詞,副詞化",
+      "記号,句点",
+      "記号,読点",
+      "記号,一般",
     ];
 
-    let mut results = vec!["".to_string()];
+    // second: これが含まれており、かつ行の後半なら改行
+    let second = vec![
+      "名詞,副詞可能",
+      "助詞,格助詞",
+      "助詞,接続助詞",
+      "助詞,副助詞",
+      "助詞,副助詞／並立助詞／終助詞",
+      "助詞,並立助詞",
+      "助詞,連体化",
+      "記号,括弧閉",
+      "フィラー",
+    ];
+
+    // forbid: ただし、これが含まれていたら改行しない
+    let forbid = ["未然形,"];
+
+    // add_before: これが含まれていたら、前の行に追加
+    let add_before = vec![
+      "非自立,",
+      "接続助詞,",
+      "終助詞,",
+      "格助詞,",
+      "句点,",
+      "読点,",
+    ];
+
+    // "助詞,格助詞,一般,*,*,*,を"
+
+    let mut results: Vec<Block> = Vec::new();
     let mut result = "".to_string();
     let delim_re = "\x1f$1";
     let delim = "\x1f";
@@ -76,13 +138,9 @@ impl Inserter {
       .collect::<Vec<String>>();
 
     for line in lines {
-      println!("{}", line);
       let sakura_script_re =
         Regex::new(r###"\\_{0,2}[a-zA-Z0-9*!&](\d|\[("([^"]|\\")+?"|([^\]]|\\\])+?)+?\])?"###)
           .unwrap();
-
-      let mut after_pre_pos = false;
-      let mut after_sahen_noun = false;
 
       let mut sakura_scripts = sakura_script_re.find_iter(&line);
       let ss_splitted = sakura_script_re.split(&line).collect::<Vec<&str>>();
@@ -90,45 +148,61 @@ impl Inserter {
         worker.reset_sentence(pieces);
         worker.tokenize();
         for token in worker.token_iter() {
-          let info: Vec<&str> = token.feature().split(',').collect();
-          let pos = info[0];
-          let pos_detail = info[1];
-          let pos_katsuyou = info[4];
+          let rank = if forbid.iter().find(|&&p| token.feature().find(p).is_some()) != None {
+            Rank::Normal
+          } else if first.iter().find(|&&p| token.feature().find(p).is_some()) != None {
+            Rank::First
+          } else if second.iter().find(|&&p| token.feature().find(p).is_some()) != None {
+            Rank::Second
+          } else {
+            Rank::Normal
+          };
 
-          let no_break = hinshi_target.iter().find(|&&p| p == pos) == None
-            || pos_detail.find("接尾") != None
-            || (pos == "動詞" && pos_detail.find("サ変接続") != None)
-            || pos_detail.find("非自立") != None
-            || after_pre_pos
-            || (after_sahen_noun && pos_detail.find("サ変動詞") != None)
-            || pos_detail.find("括弧閉") != None
-            || pos_detail.find("ナイ形容詞語幹") != None
-            || pos_katsuyou.find("特殊・ナイ") != None
-            || pos_katsuyou.find("特殊・タ") != None;
-
-          if !no_break {
-            results.push(result);
-            result = "".to_string();
+          if add_before
+            .iter()
+            .find(|&&p| token.feature().find(p).is_some())
+            != None
+            && result.is_empty()
+          {
+            if results.len() > 0 {
+              let last = results.len() - 1;
+              // results[last].text += "#";
+              results[last].text += token.surface();
+            }
+          } else {
+            result += token.surface();
+            if rank != Rank::Normal {
+              if result.chars().count() == 1 {
+                // TODO: 1文字でいいのか検討
+                let last = results.len() - 1;
+                results[last].text += &result;
+                results[last].rank = rank;
+              } else {
+                results.push(Block { rank, text: result });
+              }
+              result = "".to_string();
+            }
           }
-
-          result += token.surface();
-
-          after_pre_pos = pos == "接頭詞";
-          after_sahen_noun = pos_detail.find("サ変接続") != None;
         }
-        if let Some(s) = sakura_scripts.nth(0) {
-          println!("sakura_script: {}", s.as_str());
+        if let Some(s) = sakura_scripts.next() {
           result += s.as_str();
         }
       }
-      results.push(result);
+      results.push(Block {
+        rank: Rank::Manual,
+        text: result,
+      });
       result = "".to_string();
     }
+
+    for r in results.iter() {
+      println!("{:?}: {}", r.rank, r.text);
+    }
+
     results
   }
 
-  fn render(&mut self, parts: Vec<String>) -> String {
-    println!("rendering");
+  fn render(&mut self, parts: Vec<Block>) -> String {
     let re_open_bracket = Regex::new(r"[「『（【]").unwrap();
     let re_close_bracket = Regex::new(r"[」』）】]").unwrap();
     let re_periods = Regex::new(r"[、。！？]").unwrap();
@@ -144,53 +218,55 @@ impl Inserter {
       if i >= parts.len() {
         break;
       }
-      let part = &parts[i];
+      let rank = &parts[i].rank;
+      let part = parts[i].text.clone();
       let c = self.count(part.to_string());
-      brackets_depth += re_open_bracket.find_iter(part).count() as i32;
-      brackets_depth -= (re_close_bracket.find_iter(part).count() as i32).max(0);
+      brackets_depth += re_open_bracket.find_iter(&part).count() as i32;
+      brackets_depth -= (re_close_bracket.find_iter(&part).count() as i32).max(0);
 
-      if re_change_scope.is_match(part) {
-        let c = re_change_scope.captures(part).unwrap()[0].to_string();
+      if re_change_scope.is_match(&part) {
+        let c = re_change_scope.captures(&part).unwrap()[0].to_string();
         scope = re_not_number.replace_all(&c, "").parse::<usize>().unwrap();
       }
 
-      if re_change_line.is_match(part) {
+      if re_change_line.is_match(&part) {
         counts[scope] = 0.0;
       }
 
-      println!(
-        "count: {}, c: {}, cols_num: {}",
-        counts[scope], c, self.cols_num
-      );
       if c > self.cols_num {
-        result.push_str(part);
+        result.push_str(&part);
         counts[scope] += c % self.cols_num;
         counts[scope] %= self.cols_num;
         i += 1;
         continue;
       }
-      if counts[scope] + c > self.cols_num {
+      let after_counts = counts[scope] + c;
+      if after_counts > self.cols_num && *rank == Rank::First {
+        // result.push_str(&format!("f{}\\n", counts[scope]));
         result.push_str("\\n");
         counts[scope] = 0.0;
-        println!("continue: {}", counts[scope] + c);
+        continue;
+      } else if after_counts > self.cols_num && *rank == Rank::Second {
+        // result.push_str(&format!("s{}\\n", counts[scope]));
+        result.push_str("\\n");
+        counts[scope] = 0.0;
         continue;
       }
       counts[scope] += c;
-      println!("ok: {}", i);
-      result.push_str(part);
+      result.push_str(&part);
 
       // 句読点後の文章が1行に収まるなら、一気に出力して次へ
       // ただし括弧内では実行しない ぶつ切りの引用文とか台詞はなんか変な感じがするので
-      if re_periods.is_match(part) && brackets_depth == 0 {
+      if re_periods.is_match(&part) && brackets_depth == 0 {
         let mut j = i + 1;
         let mut next_line = String::new();
         while j < parts.len() {
-          let next = &parts[j];
-          if re_change_scope.is_match(next) || re_change_line.is_match(next) {
+          let next = parts[j].text.clone();
+          if re_change_scope.is_match(&next) || re_change_line.is_match(&next) {
             j -= 1;
             break;
           }
-          next_line.push_str(next);
+          next_line.push_str(&next);
           j += 1;
         }
 
@@ -198,8 +274,11 @@ impl Inserter {
         if next_word_count <= self.cols_num {
           // 句読点の後に改行を入れるのは、句読点の後の文章が1行に収まらない場合のみ
           if counts[scope] + next_word_count > self.cols_num {
+            // result.push_str("@\\n");
             result.push_str("\\n");
             counts[scope] = 0.0;
+          } else {
+            // result.push_str(";");
           }
           result.push_str(&next_line);
           counts[scope] += next_word_count;
@@ -226,21 +305,19 @@ impl Inserter {
 #[cfg(test)]
 mod tests {
   use super::*;
+  use crate::events::aitalk::TALKS;
 
   #[test]
   fn inserter() {
-    let text = "\
-            \\s[1111105]人生に変化は付きもの……けれど、\\s[1111109]停滞はそれ以上。\\n\
-            一度立ち止まってしまうと、空気は一瞬で淀んで、身動きがとれなくなってしまうのよ。\
-            あなたも経験したこと、あるんじゃないかしら。\\n\
-            h1111205ある詩人が言っていたのよ。「死は必ずしも最悪の運命ではない。h1111209もっと恐ろしい運命がある」と。\
-            "
-            .to_string();
-    let mut ins = Inserter::default();
-    ins.start_init();
-    while !ins.is_ready() {
-      std::thread::sleep(std::time::Duration::from_millis(100));
+    for t in TALKS.iter() {
+      let text = t.to_string();
+      let mut ins = Inserter::default();
+      ins.start_init();
+      while !ins.is_ready() {
+        std::thread::sleep(std::time::Duration::from_millis(100));
+      }
+      ins.tokenize(text.clone());
+      println!("\n{}\n", ins.run(text).replace("\\n", "\n"));
     }
-    println!("{}", ins.run(text).replace("\\n", "\n"));
   }
 }
