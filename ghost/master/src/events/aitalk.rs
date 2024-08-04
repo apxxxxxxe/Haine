5a904c21c5234651958ed85dd42a350ffc754fcd
apxxxxxxe <calcium629@gmail.com>
2024-08-04 22:16:37 +0900

    ✅ トーク推敲の影響でテストが失敗していたのを修正

--- ghost/master/src/events/aitalk.rs
+++ ghost/master/src/events/aitalk.rs
@@ -254,7 +254,7 @@ mod test {
 
   #[test]
   fn test_firstboot_flags() -> Result<(), Box<dyn std::error::Error>> {
-    const FIRST_CLOSE_TALK_PART: &str = "逃がして良いのかって？";
+    const FIRST_CLOSE_TALK_PART: &str = "私の側にいてほしい";
     const SECOND_CLOSE_TALK_PART: &str = "がありますように";
     const CLOSE_TALK_IN_LIBRARY_PART: &str = "戻ってきたようだ。";
     const PAST_TALK_UNLOCK_TALK_PART: &str = "あの子は生者だから";
