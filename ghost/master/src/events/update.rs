462fa34224f7fb8fe1dd789db802512b4a152ae0
apxxxxxxe <calcium629@gmail.com>
2025-05-24 15:28:35 +0900

    🚸 更新時に各アイテムの更新結果を一覧で表示するように

--- ghost/master/src/events/update.rs
+++ ghost/master/src/events/update.rs
@@ -3,19 +3,9 @@ use shiorust::message::{Request, Response};
 
 pub(crate) fn on_update_begin(_req: &Request) -> Response {
   new_response_with_value_with_notranslate(
-    "\\1\\_q更新を確認中……".to_string(),
+    "\\1\\_qゴーストの更新を確認中……\\n".to_string(),
     TranslateOption::none(),
   )
-}
-
-pub(crate) fn on_update_complete(req: &Request) -> Response {
-  let refs = get_references(req);
-  let m = match refs[0] {
-    "none" => "更新なし",
-    "changed" => "更新完了",
-    _ => &format!("不明なステータス({})", refs[0]),
-  };
-  new_response_with_value_with_notranslate(format!("\\1\\_q{}", m), TranslateOption::none())
 }
 
 pub(crate) fn on_update_other_begin(req: &Request) -> Response {
@@ -29,7 +19,7 @@ pub(crate) fn on_update_other_begin(req: &Request) -> Response {
     _ => &format!("不明なアイテム({})", render_refs(&refs)),
   };
   new_response_with_value_with_notranslate(
-    format!("\\1\\_q{}の更新を確認中……", item_type),
+    format!("\\C\\1\\_q{}の更新を確認中……\\n", item_type),
     TranslateOption::none(),
   )
 }
@@ -38,23 +28,38 @@ fn render_refs(refs: &Vec<&str>) -> String {
   refs.iter().map(|x| format!("{}, ", x)).collect::<String>()
 }
 
-pub(crate) fn on_update_other_complete(req: &Request) -> Response {
+pub(crate) fn on_update_result_ex(req: &Request) -> Response {
   let refs = get_references(req);
-  let item_type = match refs[3] {
-    "ghost" => "ゴースト",
-    "shell" => "シェル",
-    "balloon" => "バルーン",
-    "plugin" => "プラグイン",
-    "headline" => "ヘッドライン",
-    _ => &format!("不明なアイテム({})", render_refs(&refs)),
-  };
-  let m = match refs[0] {
-    "none" => "更新なし",
-    "changed" => "更新完了",
-    _ => &format!("不明なステータス({})", refs[0]),
-  };
+  let mut m = String::new();
+  for r in refs.iter() {
+    let results = r.split(1 as char).collect::<Vec<&str>>();
+    if results.len() < 4 {
+      continue;
+    }
+    let item_name = results[0];
+    let item_type = match results[1] {
+      "ghost" => "ゴースト",
+      "shell" => "シェル",
+      "balloon" => "バルーン",
+      "plugin" => "プラグイン",
+      "headline" => "ヘッドライン",
+      _ => &format!("不明なアイテム({})", render_refs(&refs)),
+    };
+    let status = match results[2] {
+      "OK" => match results[3] {
+        "0" => "更新なし",
+        _ => "更新成功",
+      },
+      "NG" => &format!("更新失敗({})", results[3]),
+      _ => &format!("不明なステータス({})", refs[0]),
+    };
+    m.push_str(&format!("{}({}): {}\\n", item_name, item_type, status));
+  }
   new_response_with_value_with_notranslate(
-    format!("\\1\\_q{}の{}", item_type, m),
+    format!(
+      "\\1\\_q{}{}",
+      m, "\\n\\![*]\\q[更新履歴,https://apxxxxxxe.dev/works]\\x"
+    ),
     TranslateOption::none(),
   )
 }
